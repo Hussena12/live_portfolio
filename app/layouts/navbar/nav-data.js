e8806bf9401ce34d64dc456dc6f59d2416fd5193
Hussen Ali <ha0959943@gmail.com>
2025-05-06 08:25:04 +0300

    initial commit

---
+++ app/layouts/navbar/nav-data.js
@@ -0,0 +1,38 @@
+import config from '~/config.json';
+
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+  {
+    label: 'Articles',
+    pathname: '/articles',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Bluesky',
+    url: `https://mail.google.com/${config.bluesky}`,
+    icon: 'bluesky',
+  },
+  {
+    label: 'Figma',
+    url: `https://www.linkedin.com/${config.figma}`,
+    icon: 'figma',
+  },
+  {
+    label: 'Github',
+    url: `https://github.com/${config.github}`,
+    icon: 'github',
+  },
+];
---
+++ app/routes/projects.slice/slice.jsx
@@ -0,0 +1,205 @@
+import sliceAnnotationLarge from '~/assets/slice-annotation-large.png';
+import sliceAnnotationPlaceholder from '~/assets/slice-annotation-placeholder.png';
+import sliceAnnotation from '~/assets/slice-annotation.png';
+import sliceAppLarge from '~/assets/slice-app-large.jpg';
+import sliceAppPlaceholder from '~/assets/slice-app-placeholder.jpg';
+import sliceApp from '~/assets/slice-app.jpg';
+import sliceBackgroundBarLarge from '~/assets/slice-background-bar-large.jpg';
+import sliceBackgroundBarPlaceholder from '~/assets/slice-background-bar-placeholder.jpg';
+import sliceBackgroundBar from '~/assets/slice-background-bar.jpg';
+import sliceBackgroundLarge from '~/assets/slice-background-large.jpg';
+import sliceBackgroundPlaceholder from '~/assets/slice-background-placeholder.jpg';
+import sliceBackground from '~/assets/slice-background.jpg';
+import sliceIrlPlaceholder from '~/assets/slice-irl-placeholder.jpg';
+import sliceIrl from '~/assets/slice-irl.jpg';
+import sliceSidebarAnnotationsLarge from '~/assets/slice-sidebar-annotations-large.png';
+import sliceSidebarAnnotationsPlaceholder from '~/assets/slice-sidebar-annotations-placeholder.png';
+import sliceSidebarAnnotations from '~/assets/slice-sidebar-annotations.png';
+import sliceSidebarLayersLarge from '~/assets/slice-sidebar-layers-large.png';
+import sliceSidebarLayersPlaceholder from '~/assets/slice-sidebar-layers-placeholder.png';
+import sliceSidebarLayers from '~/assets/slice-sidebar-layers.png';
+import sliceSlidesLarge from '~/assets/slice-slides-large.jpg';
+import sliceSlidesPlaceholder from '~/assets/slice-slides-placeholder.jpg';
+import sliceSlides from '~/assets/slice-slides.jpg';
+import { Footer } from '~/components/footer';
+import { Image } from '~/components/image';
+import {
+  ProjectBackground,
+  ProjectContainer,
+  ProjectHeader,
+  ProjectImage,
+  ProjectSection,
+  ProjectSectionColumns,
+  ProjectSectionContent,
+  ProjectSectionHeading,
+  ProjectSectionText,
+  ProjectTextRow,
+} from '~/layouts/project';
+import { Fragment } from 'react';
+import { media } from '~/utils/style';
+import { baseMeta } from '~/utils/meta';
+import styles from './slice.module.css';
+
+const title = 'Laboratory System ';
+const description =
+  'This project involved building a digital system for managing lab tests, results, appointments, and diagnostic equipment integration.';
+const roles = ['User Research', 'UX Design', 'Interface Design'];
+
+export const meta = () => {
+  return baseMeta({ title, description, prefix: 'Projects' });
+};
+
+export const Slice = () => {
+  return (
+    <Fragment>
+      <ProjectContainer className={styles.slice}>
+        <ProjectBackground
+          src={sliceBackground}
+          srcSet={`${sliceBackground} 1280w, ${sliceBackgroundLarge} 2560w`}
+          width={1280}
+          height={800}
+          placeholder={sliceBackgroundPlaceholder}
+          opacity={0.8}
+        />
+        <ProjectHeader
+          title={title}
+          description={description}
+          url="https://www.arsholab.com"
+          roles={roles}
+        />
+        <ProjectSection padding="top">
+          <ProjectSectionContent>
+            <ProjectImage
+              srcSet={`${sliceApp} 800w, ${sliceAppLarge} 1920w`}
+              width={800}
+              height={500}
+              placeholder={sliceAppPlaceholder}
+              alt="The Slice web application showing a selected user annotation."
+              sizes={`(max-width: ${media.mobile}px) 100vw, (max-width: ${media.tablet}px) 90vw, 80vw`}
+            />
+          </ProjectSectionContent>
+        </ProjectSection>
+        <ProjectSection>
+          <ProjectSectionColumns centered className={styles.columns}>
+            <div className={styles.imagesText}>
+              <ProjectSectionHeading>Integrating Health Systems</ProjectSectionHeading>
+              <ProjectSectionText>
+                Arsho needed a streamlined system to manage lab appointments, results, and
+                equipment integration. Previously, processes were manual and disconnected,
+                leading to inefficiencies in diagnostics and patient care.
+              </ProjectSectionText>
+              <ProjectSectionText>
+                Our solution was to develop an integrated platform enabling patients to
+                schedule appointments, access lab results, and streamline diagnostic
+                workflows.
+              </ProjectSectionText>
+            </div>
+            <div className={styles.sidebarImages}>
+              <Image
+                className={styles.sidebarImage}
+                srcSet={`${sliceSidebarLayers} 350w, ${sliceSidebarLayersLarge} 700w`}
+                width={350}
+                height={750}
+                placeholder={sliceSidebarLayersPlaceholder}
+                alt="The layers sidebar design, now with user profiles."
+                sizes={`(max-width: ${media.mobile}px) 200px, 343px`}
+              />
+              <Image
+                className={styles.sidebarImage}
+                srcSet={`${sliceSidebarAnnotations} 350w, ${sliceSidebarAnnotationsLarge} 700w`}
+                width={350}
+                height={750}
+                placeholder={sliceSidebarAnnotationsPlaceholder}
+                alt="Multiple user annotations on a shared layer."
+                sizes={`(max-width: ${media.mobile}px) 200px, 343px`}
+              />
+            </div>
+          </ProjectSectionColumns>
+        </ProjectSection>
+        <ProjectSection light>
+          <ProjectSectionContent>
+            <ProjectTextRow>
+              <ProjectSectionHeading>Enhancing User Accessibility</ProjectSectionHeading>
+              <ProjectSectionText>
+                Users often faced challenges in tracking their lab appointments and
+                accessing past test results. To address this, we introduced a personalized
+                dashboard displaying upcoming appointments and a comprehensive history of
+                previous tests. Additionally, we implemented a feature allowing users to
+                mark frequently accessed tests as favorites, facilitating quicker
+                retrieval and improved user experience.
+              </ProjectSectionText>
+            </ProjectTextRow>
+            <Image
+              srcSet={`${sliceSlides} 800w, ${sliceSlidesLarge} 1920w`}
+              width={800}
+              height={500}
+              placeholder={sliceSlidesPlaceholder}
+              alt="The new My Slides tab in slice, showing annotated and favorited slides."
+              sizes={`(max-width: ${media.mobile}px) 500px, (max-width: ${media.tablet}px) 800px, 1000px`}
+            />
+          </ProjectSectionContent>
+        </ProjectSection>
+        <ProjectSection padding="top">
+          <ProjectSectionContent className={styles.grid}>
+            <div className={styles.gridImage}>
+              <div className={styles.gridBackground}>
+                <Image
+                  srcSet={`${sliceBackgroundBar} 440w, ${sliceBackgroundBarLarge} 880w`}
+                  width={440}
+                  height={790}
+                  placeholder={sliceBackgroundBarPlaceholder}
+                  alt=""
+                  role="presentation"
+                  sizes={`(max-width: ${media.mobile}px) 312px, (max-width: ${media.tablet}px) 408px, 514px`}
+                />
+              </div>
+              <div className={styles.gridForeground}>
+                <Image
+                  srcSet={`${sliceAnnotation} 440w, ${sliceAnnotationLarge} 880w`}
+                  width={440}
+                  height={340}
+                  placeholder={sliceAnnotationPlaceholder}
+                  alt="An annotation preview popover with statistics for shape perimeter and area."
+                  sizes={`(max-width: ${media.mobile}px) 584px, (max-width: ${media.tablet}px) 747px, 556px`}
+                />
+              </div>
+            </div>
+            <div className={styles.gridText}>
+              <ProjectSectionHeading>Meaningful details</ProjectSectionHeading>
+              <ProjectSectionText>
+                Users often found it challenging to interpret lab results without
+                contextual information. To address this, we integrated reference ranges
+                and visual indicators into result displays, providing clarity on normal
+                versus abnormal values. Additionally, we implemented tooltips and
+                explanatory notes to assist users in understanding complex medical
+                terminology, enhancing overall user comprehension.
+              </ProjectSectionText>
+            </div>
+          </ProjectSectionContent>
+        </ProjectSection>
+        <ProjectSection>
+          <ProjectSectionContent>
+            <ProjectTextRow>
+              <ProjectSectionHeading>Project outcomes</ProjectSectionHeading>
+              <ProjectSectionText>
+                Implementing real-time digital tools at Arsho Medical Laboratories
+                enhanced collaboration among staff and improved patient engagement. The
+                new system facilitated seamless sharing of lab results and streamlined
+                appointment scheduling, leading to increased efficiency and user
+                satisfaction.
+              </ProjectSectionText>
+            </ProjectTextRow>
+            <Image
+              src={sliceIrl}
+              width={940}
+              height={500}
+              placeholder={sliceIrlPlaceholder}
+              alt="Students at the University of New South Wales using the new collaborative annotation features"
+            />
+          </ProjectSectionContent>
+        </ProjectSection>
+      </ProjectContainer>
+      <Footer />
+    </Fragment>
+  );
+};
